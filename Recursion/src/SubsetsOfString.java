14d42bdca013b5c7a490f5c23c1f3a65335f392d
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-05-13 12:01:24 +0530

    SubsetsOfString

--- Recursion/src/SubsetsOfString.java
+++ Recursion/src/SubsetsOfString.java
@@ -1,15 +1,16 @@
 public class SubsetsOfString {
-
-    public static void main(String[] args) {
-        System.out.println("Subsets of 'ABC':");
-        subsetsString("ABC", "");
-    }
-    public static void subsetsString(String str, String current) {
-        if (str.isEmpty()) {
+    public static void generateSubsets(String str, String current, int index) {
+        if (index == str.length()) {
             System.out.println(current);
             return;
         }
-        subsetsString(str.substring(1), current + str.charAt(0));
-        subsetsString(str.substring(1), current);
+        generateSubsets(str, current + str.charAt(index), index + 1);
+        generateSubsets(str, current, index + 1);
     }
+    public static void main(String[] args) {
+        String str = "ABC";
+        System.out.println("Subsets of '" + str + "':");
+        generateSubsets(str, "", 0);
+    }
+
 }
