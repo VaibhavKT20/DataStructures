24ee931c7f76cbf98949957985fc052a87453bc4
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-04 10:04:43 +0530

    String1

--- String/src/String1.java
+++ String/src/String1.java
@@ -1,16 +1,51 @@
 public class String1 {
     public static void main(String[] args) {
-        String name="Vaibhav";
-        System.out.println(name);
-        int arr[]=new int[3];
-        arr[0]=4;
-        System.out.println(arr[0]);
-        int brr[]=arr;
-        brr[0]=8;
-        System.out.println(brr[0]);
-        System.out.println(arr[0]);
-        int crr[]=new int[3];
-        crr[0]=2;
-        System.out.println(crr[0]);
+//        String name1="Vaibhav";
+//        System.out.println(name1);
+//        int arr[]=new int[3];
+//        arr[0]=4;
+//        System.out.println(arr[0]);
+//        int brr[]=arr;
+//        brr[0]=8;
+//        System.out.println(brr[0]);
+//        System.out.println(arr[0]);
+//        int crr[]=new int[3];
+//        crr[0]=2;
+//        System.out.println(crr[0]);
+//        String name2=name1;
+//        name2="Amit";
+//        String name3="Rahul";
+//        System.out.println(name1);
+//        System.out.println(name2);
+//        System.out.println(name3);
+
+        // ==
+//        String name1="Vaibhav";
+//        String name2="Vaibhav";
+//        String name3="Vaibhav";
+//        String name4=name1;
+//        System.out.println(name1==name2);
+//        System.out.println(name2==name3);
+//        System.out.println(name3==name4);
+//        System.out.println(name4==name2);
+//        System.out.println(name1==name4);
+
+//        String object
+        String name1=new String("Vaibhav");
+        String name2=new String("Vaibhav");
+        String name3=new String("Vaibhav");
+        String name4=name1;
+        System.out.println(name1==name2);
+        System.out.println(name2==name3);
+        System.out.println(name3==name4);
+        System.out.println(name4==name2);
+        System.out.println(name1==name4);
+        System.out.println();
+        System.out.println(name1.equals(name2));
+        System.out.println(name2.equals(name3));
+        System.out.println(name3.equals(name4));
+        System.out.println(name1.equals(name4));
+
+
     }
 }
