0ccb382f825305125888e587c482ede5d15977d4
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-04 10:20:54 +0530

    String2

--- String/src/String2.java
+++ String/src/String2.java
@@ -1,15 +1,21 @@
 public class String2 {
     public static void main(String[] args) {
-        String str="Hello World";
-        System.out.println(str.length());
-        System.out.println(str.toLowerCase());
-        System.out.println(str.toUpperCase());
-        System.out.println(str.equals("Hello World"));
-        System.out.println(str.compareTo("Hello W"));
-        System.out.println("Hello".compareTo(str));
-        System.out.println(str.compareTo("Hello World"));
-        System.out.println(str.compareTo("Aello World"));
-        System.out.println(str.compareTo("Lello World"));
+//        String str="Hello World";
+//        System.out.println(str.length());
+//        System.out.println(str.toLowerCase());
+//        System.out.println(str.toUpperCase());
+//        System.out.println(str.equals("Hello World"));
+//        System.out.println(str.compareTo("Hello W"));
+//        System.out.println("Hello".compareTo(str));
+//        System.out.println(str.compareTo("Hello World"));
+//        System.out.println(str.compareTo("Aello World"));
+//        System.out.println(str.compareTo("Lello World"));
+
+        String locations="Delhi, Chennai, Bangalore, Hyderabad, Noida, Pune";
+        String myLocationList[]=locations.split(", ");
+        for(int i=0;i<myLocationList.length;i++){
+            System.out.println(myLocationList[i]+" ");
+        }
 
     }
 }
