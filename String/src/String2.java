8aea407fdd2f7433a2ce12f9633b4f74f1d9a5a1
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-04 10:31:46 +0530

    String2

--- String/src/String2.java
+++ String/src/String2.java
@@ -11,11 +11,24 @@ public class String2 {
 //        System.out.println(str.compareTo("Aello World"));
 //        System.out.println(str.compareTo("Lello World"));
 
-        String locations="Delhi, Chennai, Bangalore, Hyderabad, Noida, Pune";
-        String myLocationList[]=locations.split(", ");
-        for(int i=0;i<myLocationList.length;i++){
-            System.out.println(myLocationList[i]+" ");
-        }
+//        String locations="Delhi, Chennai, Bangalore, Hyderabad, Noida, Pune";
+//        String myLocationList[]=locations.split(", ");
+//        for(int i=0;i<myLocationList.length;i++){
+//            System.out.println(myLocationList[i]+" ");
+//        }
+
+        //problem
+        String email1="randomuser1@gmail.com";
+        String email2="randomuser2@hotmail.com";
+        int atIndex=email1.lastIndexOf("@");
+        int dotIndex=email1.lastIndexOf(".");
+        String result=email1.substring(atIndex+1,dotIndex);
+        System.out.println(result);
+
+        int atIndex2=email2.lastIndexOf("@");
+        int dotIndex2=email2.lastIndexOf(".");
+        String result2=email2.substring(atIndex2+1,dotIndex2);
+        System.out.println(result2);
 
     }
 }
