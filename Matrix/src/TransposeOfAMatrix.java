a70155cdea58db031dde66af30a707ee5404ef0b
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-19 10:45:36 +0530

    TransposeOfAMatrix

--- Matrix/src/TransposeOfAMatrix.java
+++ Matrix/src/TransposeOfAMatrix.java
@@ -1,16 +1,31 @@
 public class TransposeOfAMatrix {
+    public static void swap(int [][]matrix,int i,int j){
+        int temp=matrix[i][j];
+        matrix[i][j]=matrix[j][i];
+        matrix[j][i]=temp;
+    }
     public static void print(int [][]matrix){
         int r=matrix.length;
         int c=matrix[0].length;
-        int [][]temp=new int[r][c];
+//
+//        Naive Approach
+
+//        int [][]temp=new int[r][c];
+//        for(int i=0;i<r;i++){
+//            for(int j=0;j<c;j++){
+//                temp[i][j]=matrix[j][i];
+//            }
+//        }
+//        for(int i=0;i<r;i++){
+//            for(int j=0;j<c;j++){
+//                matrix[i][j]=temp[i][j];
+//            }
+//        }
+
+        //Optimal Approach
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                temp[i][j]=matrix[j][i];
-            }
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                matrix[i][j]=temp[i][j];
+            for(int j=i+1;j<c;j++){
+                swap(matrix,i,j);
             }
         }
         System.out.println("Transposed Matrix: ");
