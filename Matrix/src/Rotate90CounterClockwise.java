39ccb0d7b933dd914f58d99eef4839c3002c2db1
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-19 11:59:54 +0530

    Rotate90CounterClockwise

--- Matrix/src/Rotate90CounterClockwise.java
+++ Matrix/src/Rotate90CounterClockwise.java
@@ -1,16 +1,45 @@
 public class Rotate90CounterClockwise {
+    public static void swap1(int [][]mat,int i,int j){
+        int temp=mat[i][j];
+        mat[i][j]=mat[j][i];
+        mat[j][i]=temp;
+    }
+    public static void swap2(int [][]mat,int high,int low,int i){
+        int temp=mat[low][i];
+        mat[low][i]=mat[high][i];
+        mat[high][i]=temp;
+    }
     public static void print(int[][]mat){
         int r= mat.length;
         int c= mat[0].length;
-        int [][]temp=new int[r][c];
+//        int [][]temp=new int[r][c];
+//        for(int i=0;i<r;i++){
+//            for(int j=0;j<c;j++){
+//                temp[r-j-1][i]=mat[i][j];
+//            }
+//        }
+//        for(int i=0;i<r;i++){
+//            for(int j=0;j<c;j++){
+//                mat[i][j]=temp[i][j];
+//            }
+//        }
+
+//        Optimal Approach
+//        Transpose
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                temp[r-j-1][i]=mat[i][j];
+            for(int j=i+1;j<c;j++){
+                swap1(mat,i,j);
             }
         }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                mat[i][j]=temp[i][j];
+
+//        Reverse
+        for(int i=0;i<c;i++){
+            int low=0;
+            int high=c-1;
+            while(low<high){
+                swap2(mat,low,high,i);
+                low++;
+                high--;
             }
         }
 
