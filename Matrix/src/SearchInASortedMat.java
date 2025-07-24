5f05117a372cdb5d5e921a3eafa631a580b201eb
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-24 11:03:48 +0530

    SearchInASortedMat

--- Matrix/src/SearchInASortedMat.java
+++ Matrix/src/SearchInASortedMat.java
@@ -1,4 +1,22 @@
 public class SearchInASortedMat {
+    public static void searchInASortedMat(int [][]mat,int x){
+        int r=mat.length;
+        int c=mat[0].length;
+        int i=0;
+        int j=c-1;
+        while (i<r && j>=0){
+            if(mat[i][j]==x){
+                System.out.println(x+" Found at index ("+i+","+j+")");
+                return;
+            }
+            else if(mat[i][j]>x){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+    }
     public static void main(String[] args) {
         int [][]mat={{10,20,30,40},{15,25,35,45},{19,29,39,49},{48,46,44,42}};
         System.out.println("Original Matrix");
