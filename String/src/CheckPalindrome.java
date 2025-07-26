7ec03a182285010a148ceddc4f25373b6fe404d8
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-26 14:55:34 +0530

    CheckPalindrome

--- String/src/CheckPalindrome.java
+++ String/src/CheckPalindrome.java
@@ -1,15 +1,21 @@
 public class CheckPalindrome {
+//    public static boolean checkPalindrome(String str){
+//        int start=0;
+//        int end=str.length()-1;
+//        while(start<end){
+//            if(str.charAt(start)!=str.charAt(end)){
+//                return false;
+//            }
+//            start++;
+//            end--;
+//        }
+//        return true;
+//    }
+
     public static boolean checkPalindrome(String str){
-        int start=0;
-        int end=str.length()-1;
-        while(start<end){
-            if(str.charAt(start)!=str.charAt(end)){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+        StringBuilder rev=new StringBuilder(str);
+        rev.reverse();
+        return str.equals(rev.toString());
     }
     public static void main(String[] args) {
         boolean result=checkPalindrome("NAMAN");
