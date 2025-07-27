6e32ae87789152c70813ff60871d633f13c948ba
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-27 10:14:59 +0530

    LeftMostRepeating

--- String/src/LeftMostRepeating.java
+++ String/src/LeftMostRepeating.java
@@ -1,15 +1,30 @@
 public class LeftMostRepeating {
-    public static int leftMostRepeating(String str){
-        int n=str.length();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(str.charAt(i)==str.charAt(j)){
-                    return i;
-                }
-            }
+//    public static int leftMostRepeating(String str){
+//        int n=str.length();
+//        for(int i=0;i<n;i++){
+//            for(int j=i+1;j<n;j++){
+//                if(str.charAt(i)==str.charAt(j)){
+//                    return i;
+//                }
+//            }
+//
+//        }
+//        return -1;
+//    }
 
+    static final int CHAR=256;
+    public static int leftMostRepeating(String str){
+        boolean []visited=new boolean[CHAR];
+        int res=-1;
+        for(int i=str.length()-1;i>=0;i--){
+            if(visited[str.charAt(i)]){
+                res=i;
+            }
+            else{
+                visited[str.charAt(i)]=true;
+            }
         }
-        return -1;
+        return res;
     }
     public static void main(String[] args) {
         String str="abbccdd";
