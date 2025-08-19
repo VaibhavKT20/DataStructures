2d239a0de3ef4dca42f02230e18c90a81af46456
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-19 09:17:31 +0530

    GetMinDiff

--- Sorting/src/GetMinDiff.java
+++ Sorting/src/GetMinDiff.java
@@ -1,11 +1,23 @@
+import java.util.Arrays;
+
 public class GetMinDiff {
+//    public static int getMinDiff(int []arr){
+//        int n=arr.length;
+//        int res=Integer.MAX_VALUE;
+//        for(int i=1;i<n;i++){
+//            for(int j=0;j<i;j++){
+//                res=Math.min(res,Math.abs(arr[i]-arr[j]));
+//            }
+//        }
+//        return res;
+//    }
+
     public static int getMinDiff(int []arr){
         int n=arr.length;
         int res=Integer.MAX_VALUE;
+        Arrays.sort(arr);
         for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                res=Math.min(res,Math.abs(arr[i]-arr[j]));
-            }
+            res=Math.min(res,(arr[i]-arr[i-1]));
         }
         return res;
     }
