3eb2dbb32c14724c02e603f109cf1d350185dfcb
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-21 11:53:49 +0530

    HashMapDemo

---
+++ Hashing/src/HashMapDemo.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.*;
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String,Integer> m=new HashMap<>();
+        m.put("ide",10);
+        m.put("course",15);
+        m.put("gfg",20);
+        System.out.println(m);
+        System.out.println(m.size());
+        for(Map.Entry<String,Integer> e:m.entrySet()){
+            System.out.println(e.getKey()+" "+e.getValue());
+        }
+    }
+}
