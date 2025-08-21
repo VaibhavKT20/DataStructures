04946a95b03edb53f0e16fd37bd82e6b382b34ee
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-21 12:33:23 +0530

    countFrequency

---
+++ Hashing/src/CountFrequency.java
@@ -0,0 +1,29 @@
+public class CountFrequency {
+    public static void countFrequency(int []arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == true) {
+                continue;
+            }
+            int freq = 1;
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    freq++;
+                }
+            }
+
+            System.out.println(arr[i]+" : "+freq);
+        }
+    }
+    public static void main(String[] args) {
+        int []arr={10,12,10,15,10,20,12,12};
+        countFrequency(arr);
+    }
+}
