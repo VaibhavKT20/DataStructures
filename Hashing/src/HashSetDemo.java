b2c0ff6a5ad671afd31184f9233d4fec4e0f9770
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-21 11:41:59 +0530

    HashSetDemo

--- Hashing/src/HashSetDemo.java
+++ Hashing/src/HashSetDemo.java
@@ -1,14 +1,21 @@
 import java.util.*;
 public class HashSetDemo {
     public static void main(String[] args) {
-        HashSet<String> h = new HashSet<String>();
+        HashSet<String> h = new HashSet<>();
         h.add("Gfg");
         h.add("Course");
         h.add("ide");
-        Iterator<String> i= h.iterator();
-        while(i.hasNext()){
-            System.out.println(i.next()+" ");
+//        Iterator<String> i= h.iterator();
+//        while(i.hasNext()){
+//            System.out.println(i.next()+" ");
+//        }
+        System.out.println(h.size());
+        h.remove("ide");
+        System.out.println(h.size());
+        for(String s:h){
+            System.out.println(s);
         }
+        System.out.println(h.isEmpty());
     }
 
 
