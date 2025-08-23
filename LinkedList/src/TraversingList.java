48d37f14bdae26f4c1b632bcf9a41308c7f1a7d8
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-23 11:38:30 +0530

    Implementation

--- LinkedList/src/TraversingList.java
+++ LinkedList/src/TraversingList.java
@@ -1,3 +1,30 @@
-public class TraversingList {
 
+public class TraversingList {
+    public static void printList(Node head){
+        Node curr=head;
+        while(curr.next!=null){
+            System.out.print(curr.data+"-> ");
+            curr= curr.next;
+        }
+        System.out.println("NULL");
+    }
+
+    //Recursive approach for the traversing the list
+    public static void rPrintList(Node head){
+        if(head==null){
+            return;
+        }
+        System.out.print(head.data+"  ");
+        rPrintList(head.next);
+
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+        printList(head);
+        rPrintList(head);
+    }
 }
